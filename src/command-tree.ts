088ea3124724739d77528421007223b5245c3e77
Jack Davenport <jack.davenport5011@gmail.com>
2022-06-26 22:54:54 +1000

    Added permission type definitions

--- src/command-tree.ts
+++ src/command-tree.ts
@@ -4,14 +4,30 @@ export type CommandTree = {
 
 export type OptionList = {
     [name: string]: Option | Subcommand | SubcommandGroup
-}
+};
+
+export type PermissionList = {
+    dm?: boolean,
+    disabled?: boolean,
+    requires?: Permission[]
+};
 
 export type Command = {
     description: string,
     options?: OptionList
 };
 
-type ChannelType = "text" | "dm" | "voice" | "group_dm" | "category" | "announcements" | "store" | "announcement_thread" | "public_thread" | "private_thread" | "stage";
+type ChannelType =
+    "text" | "dm" | "voice" | "group_dm" | "category" | "announcements" | "store" | "announcement_thread" | "public_thread" |
+    "private_thread" | "stage";
+type Permission =
+    "CREATE_INSTANT_INVITE" | "KICK_MEMBERS" | "BAN_MEMBERS" | "ADMINISTRATOR" | "MANAGE_CHANNELS" | "MANAGE_GUILD" | "ADD_REACTIONS" |
+    "VIEW_AUDIT_LOG" | "PRIORITY_SPEAKER" | "STREAM" | "VIEW_CHANNEL" | "SEND_MESSAGES" | "SEND_TTS_MESSAGES" | "MANAGE_MESSAGES" |
+    "EMBED_LINKS" | "ATTACH_FILES" | "READ_MESSAGE_HISTORY" | "MENTION_EVERYONE" | "USE_EXTERNAL_EMOJIS" | "VIEW_GUILD_INSIGHTS" |
+    "CONNECT" | "SPEAK" | "MUTE_MEMBERS" | "DEAFEN_MEMBERS" | "MOVE_MEMBERS" | "USE_VAD" | "CHANGE_NICKNAME" | "MANAGE_NICKNAMES" |
+    "MANAGE_ROLES" | "MANAGE_WEBHOOKS" | "MANAGE_EMOJIS_AND_STICKERS" | "USE_APPLICATION_COMMANDS" | "REQUEST_TO_SPEAK" | "MANAGE_EVENTS" |
+    "MANAGE_THREADS" | "USE_PUBLIC_THREADS" | "CREATE_PUBLIC_THREADS" | "USE_PRIVATE_THREADS" | "CREATE_PRIVATE_THREADS" |
+    "USE_EXTERNAL_STICKERS" | "SEND_MESSAGES_IN_THREADS" | "START_EMBEDDED_ACTIVITIES" | "MODERATE_MEMBERS";
 
 export type Option = {
     description: string,
