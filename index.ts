f7b7d7fd27463f68e9ae2bc3a8e3a4c5d25b4aff
Jack Davenport <jack.davenport5011@gmail.com>
2025-04-15 00:02:18 +1000

    [#21] Move legacy API to seperate folder

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 // just export everything from all API modules
-export * from './src/api/wrapped-client';
-export * from './src/api/command-context';
-export * from './src/api/const';
+export * from './src/legacy/api/wrapped-client';
+export * from './src/legacy/api/command-context';
+export * from './src/legacy/api/const';
