bc59229ecc72d729ec6467ce6a373608427fcac5
Jack Davenport <jack.davenport5011@gmail.com>
2025-04-23 00:48:03 +1000

    Add test for API command builder

---
+++ test/commands/api.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from "bun:test";
+import buildApiCommand from "../../src/commands/api";
+import { ApplicationCommandType, RESTPostAPIChatInputApplicationCommandsJSONBody } from "discord.js";
+
+describe("api.ts", () => {
+
+    test("buildApiCommand()", () => {
+        const builder = buildApiCommand({
+            name: "my-command",
+            description: "A new type of command",
+            execute() {}
+        });
+        expect(builder).toBeObject();
+        expect(builder.toJSON()).toMatchObject({
+            name: "my-command",
+            description: "A new type of command",
+            type: ApplicationCommandType.ChatInput
+        } satisfies RESTPostAPIChatInputApplicationCommandsJSONBody);
+    });
+
+});
